89dad177a722b48d9545c94eacbd2b18e5423dfd
ngamsomset <setthasiri.ngm@gmail.com>
2023-10-08 16:39:09 +1100

    add react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import SearchParams from './SearchParams';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Details from './Details';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <h1>Adopt Me!</h1>
+      <Link to="/">Adopt Me!</Link>
       <Routes>
         <Route path="/details/:id" element={<Details />} />
         <Route path="/" element={<SearchParams />} />
